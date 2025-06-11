fe02e175e6f4cf2b1cedb6ff0b0778e190d4b36d
Issame <i_canello@yahoo.com.br>
2025-06-11 02:15:21 -0300

    #

--- app.py
+++ app.py
@@ -9,4 +9,33 @@
 #### --> Repetir os passos acima até cadastrar todos os alunos
 
 
+import pyautogui
+import pandas
+import time
+
+pyautogui.PAUSE = 1
+pyautogui.press('win')
+pyautogui.write('chrome')
+pyautogui.press('enter')
+time.sleep(2)
+pyautogui.write('https://aula01.simplificapython.com.br')
+pyautogui.press('enter')
+# pyautogui.hotkey('win', 'up')
+time.sleep(2)
+# pyautogui.click(x=718, y=683)
+# time.sleep(1)
+pyautogui.press('tab')
+pyautogui.write('admin')
+pyautogui.press('tab')
+time.sleep(1)
+pyautogui.write('simplificapython')
+pyautogui.press('tab')
+pyautogui.press('enter')
+
+pyautogui.press('tab', presses=3)
+
+
+
+
+
 
---
+++ pega_posicao_mouse.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+
+time.sleep(5)  # Tempo para posicionar o mouse na área desejada
+
+posicao = pyautogui.position()  # Pega a posição do mouse
+print(posicao)
+
+
