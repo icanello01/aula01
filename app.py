401a49f453099007f8571d1b87738ce55526cceb
Issame <i_canello@yahoo.com.br>
2025-06-11 00:38:45 -0300

    #

---
+++ app.py
@@ -0,0 +1,12 @@
+# Instalar as bibliotecas
+# Usar as bibliotecas no meu código
+# Iniciar a automação
+## Abrir o navegador
+## Acessar o site
+## Autenticar usuário
+## Carregar o arquivo que contém os dados a serem inseridos
+## Cadastrar os alunos
+#### --> Repetir os passos acima até cadastrar todos os alunos
+
+
+
